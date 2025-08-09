e4936a4c41be77158b26167916ae8912fed47268
Adnan Shaikh <sikadnan2@gmail.com>
2025-08-09 19:32:15 +0530

    Added logo for the site and tested form

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,20 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Logo from '../assets/Adnan_Logo.png'
 
-const Navbar = ()=>{
-    return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-        <div className="collapse navbar-collapse">
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+      {/* Logo & Brand */}
+      <Link className="navbar-brand d-flex align-items-center" to="/">
+        <img
+          src={Logo}
+          alt="Logo"
+          width="40"
+          height="40"
+          className="d-inline-block align-top me-2"
+        />
+      </Link>
+      <div className="collapse navbar-collapse">
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
             <Link className="nav-link" to="/">Home</Link>
@@ -19,9 +30,9 @@ const Navbar = ()=>{
           </li>
         </ul>
       </div>
-        <hr />
-      </nav>
-    )
+      <hr />
+    </nav>
+  )
 }
 
 export default Navbar
--- frontend/src/pages/Blog.js
+++ frontend/src/pages/Blog.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { Container, Row, Col, Card } from 'react-bootstrap'
 
-const Blog = ()=>{
-    return <h2>Blog Page</h2>
+const Blog = () => {
+    const blogs = [
+        {
+            id: 1,
+            title: "My First Blog Post",
+            date: "2025-08-09",
+            description: "This is my very first blog post. I talk about my journey into web development.",
+        },
+        {
+            id: 2,
+            title: "Learning React & FastAPI",
+            date: "2025-08-10",
+            description: "Why I chose React for frontend and FastAPI for backend development.",
+        }
+    ];
+    return (
+        <Container className="mt-4">
+            <h1 className="mb-4">My Blog</h1>
+            <Row>
+                {blogs.map((blog) => (
+                    <Col md={4} key={blog.id}>
+                        <Card className="mb-4">
+                            <Card.Body>
+                                <Card.Title>{blog.title}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">{blog.date}</Card.Subtitle>
+                                <Card.Text>{blog.description}</Card.Text>
+                                <Card.Link href={`/blog/${blog.id}`}>Read More</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
 }
 
 export default Blog;
